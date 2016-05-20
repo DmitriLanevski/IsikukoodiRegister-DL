2815dc0206aa982cc90ece6610c018a47a89dd53
Dmitri Lanevski <dmitri.lanevski@ut.ee>
2016-05-20 17:16:44 +0300

    Synchronized parandatud.

--- src/IsikukoodiRegister.java
+++ src/IsikukoodiRegister.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Created by lanev_000 on 6.04.2016.
  */
 public class IsikukoodiRegister {
-    private List<String> isikukoodid = new ArrayList<>();
+    private CopyOnWriteArrayList<String> isikukoodid = new CopyOnWriteArrayList<>();
+    //private List<String> isikukoodid = new ArrayList<>();
 
     public void register(String isikukood){
         synchronized (isikukoodid) {
@@ -27,7 +30,10 @@ public class IsikukoodiRegister {
 
     public List<String> getIsikukoodid() {
         synchronized (isikukoodid){
-            return isikukoodid;
+            //It should work with CopyOnWriteArrayList. But I an not shure.
+            //Is it okey? Will sollution with ArrayList and new Arraylist(Arraylist) work?
+            return  isikukoodid;
+            //return new ArrayList<>(isikukoodid);
         }
     }
 }
